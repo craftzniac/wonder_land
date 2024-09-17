764d06f48e9e3622dd3099aeec99cbc635b9343f
MikeyOnyedika <michaelvik520@gmail.com>
2024-09-17 23:27:07 +0100

    add more test html files

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::fs;
 mod alice;
 
 fn main() {
-    let result = fs::read_to_string("test_1.html");
+    let result = fs::read_to_string("test_2.html");
     match result {
         Ok(content) => {
             // here's where the main code is;
